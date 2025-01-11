9a0a407c029d5f22fe013d2954a20a3b6a6716da
Liam Jarvis <jarviliam@gmail.com>
2025-01-11 12:39:56 +0900

    chore: run format

--- users/miryoku/config.h
+++ users/miryoku/config.h
@@ -5,8 +5,6 @@
 
 #pragma once
 
-#include "custom_config.h"
-
 // default but used in macros
 #undef TAPPING_TERM
 #define TAPPING_TERM 190
@@ -24,18 +22,18 @@
 
 // Mouse key speed and acceleration.
 #undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_DELAY 0
 #undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL       16
+#define MOUSEKEY_INTERVAL 16
 #undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_WHEEL_DELAY 0
 #undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
+#define MOUSEKEY_MAX_SPEED 6
 #undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
+#define MOUSEKEY_TIME_TO_MAX 64
 
 // Thumb Combos
-#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
-  #define COMBO_TERM 200
-  #define EXTRA_SHORT_COMBOS
+#if defined(MIRYOKU_KLUDGE_THUMBCOMBOS)
+#    define COMBO_TERM 200
+#    define EXTRA_SHORT_COMBOS
 #endif
--- users/miryoku/manna-harbour_miryoku.c
+++ users/miryoku/manna-harbour_miryoku.c
@@ -56,9 +56,9 @@ const key_override_t capsword_key_override = ko_make_basic(MOD_MASK_SHIFT, CW_TO
 const key_override_t *key_overrides[] = {&capsword_key_override, NULL};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-     // if (record->event.pressed) {
-     //     dprintf("kc=0x%04X, row=%2u, col=%2u\n", keycode, record->event.key.row, record->event.key.col);
-     // }
+    // if (record->event.pressed) {
+    //     dprintf("kc=0x%04X, row=%2u, col=%2u\n", keycode, record->event.key.row, record->event.key.col);
+    // }
 #ifdef ACHORDION_ENABLE
     if (!process_achordion(keycode, record)) {
         return false;
@@ -71,24 +71,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record, uint16_t other_keycode, keyrecord_t *other_record) {
     // Allow thumb layer same key for button macros (flash).
     // This is fixed to Layout 5_3_2, might need to adjust for 3_2_ex2
-    dprintf("key=0x%04X\n",tap_hold_keycode);
-    dprintf("other=0x%04X\n",other_keycode);
+    dprintf("key=0x%04X\n", tap_hold_keycode);
+    dprintf("other=0x%04X\n", other_keycode);
 
-// Exceptionally consider the following chords as holds, even though they
-  // are on the same hand in Dvorak.
-  // switch (tap_hold_keycode) {
-  //   case KC_ESC:  // ESC + Q.
-  //       dprintf("here");
-  //     if (other_keycode == KC_Q) { return true; }
-  //     break;
-  //
-  //   case KC_DEL:  // S + H and S + G.
-  //       dprintf("there");
-  //     if (other_keycode == KC_QUOT) { return true; }
-  //     break;
-  // }
+    // Exceptionally consider the following chords as holds, even though they
+    // are on the same hand in Dvorak.
+    // switch (tap_hold_keycode) {
+    //   case KC_ESC:  // ESC + Q.
+    //       dprintf("here");
+    //     if (other_keycode == KC_Q) { return true; }
+    //     break;
+    //
+    //   case KC_DEL:  // S + H and S + G.
+    //       dprintf("there");
+    //     if (other_keycode == KC_QUOT) { return true; }
+    //     break;
+    // }
     uint8_t row = other_record->event.key.row;
-    dprintf("%d\n",row);
+    dprintf("%d\n", row);
     if (row == 0 || row == 4) {
         return true;
     }
